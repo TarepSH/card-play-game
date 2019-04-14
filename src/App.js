278c8a92c9e7fc3b2f44564c21e2a3ece0b652be
TarepSh <tarepsh@gmail.com>
2019-04-14 23:38:37 +0300

    updat

--- src/App.js
+++ src/App.js
@@ -15,14 +15,21 @@ class GnreatCard extends React.Component{
 class Player extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { random: Math.floor(Math.random() * Math.floor(11)) };
+    this.state = { 
+      random: Math.floor(Math.random() * Math.floor(11)),
+      Playerscore: 0, 
+      
+      
+    };
   }
 
   render(){
+    let winer = "test"
     return(
       <div className={"player" + this.props.plyarenumber}>
         <h3>Player {this.props.plyarenumber}</h3>
-         <a href="#"><GnreatCard number = {this.state.random} /></a> 
+         <a href="#" onClick={() => this.setState({Playerscore: this.state.random})}><GnreatCard number = {this.state.random} /></a> 
+        <h3>plyaer socre is: {this.state.Playerscore}</h3>
       </div>
     );
   }
@@ -33,10 +40,11 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <h1>the winner is: {this.props.winer} </h1>
         <Player plyarenumber="1" />
         <Player plyarenumber="2" />
         <Player plyarenumber="3" />
-        <Player plyarenumber="4" />
+        <Player plyarenumber="4" />        
       </div>
     );
   }
